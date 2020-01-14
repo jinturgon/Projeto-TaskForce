78e3b7c9fd30b3ba4c71256a343f5a33933cc11a
Fábio Donizete <fabio.donizete@yahoo.com.br>
2020-01-14 15:40:21 -0300

    Last Commit

---
+++ include/php/faleconosco.php
@@ -0,0 +1,18 @@
+﻿<?php include("conexao.php"); ?>
+<?php
+$nome=$_POST['nome'];
+$email=$_POST['email'];
+$fone=$_POST['fone'];
+$msg=$_POST['msg'];
+
+$sql = "INSERT INTO fale_conosco (id, nome, email, fone, msg) VALUES ('', '$nome', '$email', '$fone', '$msg')";
+
+        
+if($query_result = mysqli_query($con,$sql))
+{
+	echo  nl2br ("Formulário de Contato enviado! \n Nós entraremos em contato em breve.");
+}else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+}
+$con->close();
+?>
